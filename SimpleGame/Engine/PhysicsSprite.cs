e9831e1790cd0eaeedce4a90558929c6127cb48c
Gabriel Bigardi <gabrielbigardigames@gmail.com>
2026-04-03 19:17:55 -0300

    Fix bad physics sprite code

--- SimpleGame/Engine/PhysicsSprite.cs
+++ SimpleGame/Engine/PhysicsSprite.cs
@@ -5,32 +5,29 @@ namespace SimpleGame.Engine;
 
 public class PhysicsSprite : Sprite
 {
-    public Vector2 ColliderSize;
-    public Vector2 CollisionOffset;
+    private readonly Vector2 _colliderSize;
+    private readonly Vector2 _collisionOffset;
 
-    // Added collisionOffset with a default value so existing code won't break
     public PhysicsSprite(Texture2D texture, Vector2 position, Vector2 scale, Vector2 colliderSize, Vector2 collisionOffset = default, float rotation = 0) :
         base(texture, position, scale, rotation)
     {
-        ColliderSize = colliderSize;
-        CollisionOffset = collisionOffset;
+        _colliderSize = colliderSize;
+        _collisionOffset = collisionOffset;
     }
 
     public bool CollidesWith(PhysicsSprite other)
     {
-        // Calculate the actual top-left positions of both colliders taking the offset into account
-        var myColliderPos = Position + CollisionOffset;
-        var otherColliderPos = other.Position + other.CollisionOffset;
+        var myColliderPos = Position + _collisionOffset;
+        var otherColliderPos = other.Position + other._collisionOffset;
 
-        // Perform the AABB collision check using the adjusted offset positions
-        return otherColliderPos.X < myColliderPos.X + ColliderSize.X &&
-               myColliderPos.X < otherColliderPos.X + other.ColliderSize.X &&
-               otherColliderPos.Y < myColliderPos.Y + ColliderSize.Y &&
-               myColliderPos.Y < otherColliderPos.Y + other.ColliderSize.Y;
+        return otherColliderPos.X < myColliderPos.X + _colliderSize.X &&
+               myColliderPos.X < otherColliderPos.X + other._colliderSize.X &&
+               otherColliderPos.Y < myColliderPos.Y + _colliderSize.Y &&
+               myColliderPos.Y < otherColliderPos.Y + other._colliderSize.Y;
     }
 
     public void DrawDebug(SpriteBatch spriteBatch, Color color)
     {
-        spriteBatch.DrawRectangle(Position - ColliderSize / 2f + CollisionOffset, ColliderSize, color, 2f);
+        spriteBatch.DrawRectangle(Position - _colliderSize / 2f + _collisionOffset, _colliderSize, color, 2f);
     }
 }
